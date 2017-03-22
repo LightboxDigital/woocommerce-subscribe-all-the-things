625810b41ef6b0f8a555b88e7fbb012911a5a0d7
Warren Bickley <warren@lightboxdigital.co.uk>
2017-03-22 15:16:54 +0000

    Modified JS to reload cart on payment plan change, temporary fix as sadly dynamic loading over AJAX produced weird results.

--- assets/js/wcsatt-cart.js
+++ assets/js/wcsatt-cart.js
@@ -9,8 +9,8 @@ jQuery( function( $ ) {
 	// Reload cart elements when (de-)selecting a cart subscription option.
 	$( document ).on( 'change', '.wcsatt-options-cart input[type=radio][name^=convert_to_sub]', function() {
 
-		var $cart_totals    = $( 'div.cart_totals' ),
-			$cart_table     = $( 'table.shop_table.cart' ),
+		var $cart_totals    = $( '.cart_totals' ),
+			$cart_table     = $( '.cart' ),
 			$cart_wrapper   = $cart_table.closest( '.woocommerce' ),
 			selected_option = $( this ).val(),
 			cart_referrer   = $cart_table.find( 'input[name="_wp_http_referer"]' ).val();
@@ -30,24 +30,23 @@ jQuery( function( $ ) {
 		};
 
 		$.post( wcsatt_cart_params.wc_ajax_url.toString().replace( '%%endpoint%%', 'wcsatt_update_cart_option' ), data, function( response ) {
+			// Work around reload bug with wrong prices
+			window.location.reload(true);
 
-			var $response        = $( $.parseHTML( response ) ),
-				$response_totals = $response.find( 'div.cart_totals' ),
-				$response_table  = $response.find( 'table.shop_table.cart' );
-
-			if ( cart_referrer ) {
-				$response_table.find( 'input[name="_wp_http_referer"]' ).val( cart_referrer );
-			}
-
-			if ( 'yes' === wcsatt_cart_params.is_wc_version_gte_2_6 ) {
-				$response_table.find( 'input[name="update_cart"]' ).prop( 'disabled', true );
-			}
-
-			$cart_totals.replaceWith( $response_totals );
-			$cart_table.replaceWith( $response_table );
-
-			$cart_wrapper.trigger( 'wcsatt_updated_cart' );
-			$cart_wrapper.unblock();
+			// var $response        = $( $.parseHTML( response ) );
+			//
+			// if ( cart_referrer ) {
+			// 	$response.find( 'input[name="_wp_http_referer"]' ).val( cart_referrer );
+			// }
+			//
+			// if ( 'yes' === wcsatt_cart_params.is_wc_version_gte_2_6 ) {
+			// 	$response.find( 'input[name="update_cart"]' ).prop( 'disabled', true );
+			// }
+			//
+			// $cart_table.html( $response );
+			//
+			// $cart_wrapper.trigger( 'wcsatt_updated_cart' );
+			// $cart_wrapper.unblock();
 		} );
 	} );
 } );
